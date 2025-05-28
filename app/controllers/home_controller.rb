f86c8b864125ac3076d090ce04319c5b27523fa0
janebv15 <jvaughn0@chicagobooth.edu>
2025-05-28 21:12:05 +0000

    feed update

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,14 +1,23 @@
 class HomeController < ApplicationController
-
   def index
-  render({ :template => "home/feed" })
+    recommendations = Recommendation.all.order({ :created_at => :desc })
 
+    @feed_items = recommendations.map do |recommendation|
+      {
+        :username => recommendation.user.username,
+        :category_name => recommendation.category.name,
+        :favorite_thing => recommendation.name,
+        :description => recommendation.description,
+      }
+    end
+
+    render({ :template => "home/feed" })
   end
-  
-    def profile
-    the_id         = params.fetch("id")
+
+  def profile
+    the_id = params.fetch("id")
     matching_users = User.where({ :id => the_id })
-    @user          = matching_users.at(0)
+    @user = matching_users.at(0)
 
     render({ :template => "home/profile" })
 
