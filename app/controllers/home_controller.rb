6554b509a7db7649087c7641a307b2628f0200ec
janebv15 <jvaughn0@chicagobooth.edu>
2025-05-28 22:02:45 +0000

    profile and tables

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,21 @@ class HomeController < ApplicationController
   def index
     recommendations = Recommendation.all.order({ :created_at => :desc })
 
+    # filter by username
+    username_search = params.fetch("username_search", "")
+    if username_search != ""
+      matching_users = User.where(
+        ["username LIKE ?", "%" + username_search + "%"]
+      )
+      user_ids = matching_users.pluck(:id)
+      recs = recs.where({ :user_id => user_ids })
+    end
+
+    # filter by category
+    if params.fetch("category_id", "") != ""
+      recs = recs.where({ :category_id => params.fetch("category_id") })
+    end
+
     @feed_items = recommendations.map do |recommendation|
       {
         :username => recommendation.user.username,
@@ -14,13 +29,16 @@ class HomeController < ApplicationController
     render({ :template => "home/feed" })
   end
 
-  def profile
-    the_id = params.fetch("id")
-    matching_users = User.where({ :id => the_id })
-    @user = matching_users.at(0)
+ def profile
+ #   @user = User.where({ "id" => params.fetch("user_id") }).at(0)
+  #  @category = Category.where({ "id" => params.fetch("category_id") }).at(0)
 
-    render({ :template => "home/profile" })
+   # @recommendations = Recommendation.
+    #  where({ :user_id     => @user.id,
+     #         :category_id => @category.id }).
+      #order({ :created_at  => :desc })
 
-    return
-  end
+  render({ :template => "home/profile" })
+end
+
 end
